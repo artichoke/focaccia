e6f905fb7f23f1a96d00ed3252c2887e11977fe3
artichoke-ci <ci@artichokeruby.org>
2020-08-01 18:20:14 +0000

    deploy: 2cc7f9fe07544ac88d1e1673de594b8ab6c08386

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"focaccia":{"doc":"","i":[],"p":[]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["focaccia"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
