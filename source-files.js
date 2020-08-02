84758f201a012edd837ac3882420bd53e1fd38e0
artichoke-ci <ci@artichokeruby.org>
2020-08-02 05:27:09 +0000

    deploy: a2aa1de33a54bb71f04d103562e37873bfdaaf5f

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["focaccia"] = {"name":"","dirs":[{"name":"folding","dirs":[{"name":"mapping","files":["full.rs","mod.rs","turkic.rs"]}],"files":["ascii.rs","full.rs","mod.rs","turkic.rs"]}],"files":["lib.rs"]};
+sourcesIndex["focaccia"] = {"name":"","dirs":[{"name":"folding","dirs":[{"name":"mapping","files":["lookup.rs","mod.rs"]}],"files":["ascii.rs","full.rs","mod.rs","turkic.rs"]}],"files":["lib.rs"]};
 createSourceSidebar();
