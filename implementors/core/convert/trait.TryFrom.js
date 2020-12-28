ea39b12a5398a0c3776607e76660a9b3935c05f5
artichoke-ci <ci@artichokeruby.org>
2020-12-28 03:27:20 +0000

    deploy: 8b26bbf567773a42c03bc52e5ffd590aea6f0a6a

--- implementors/core/convert/trait.TryFrom.js
+++ implementors/core/convert/trait.TryFrom.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["focaccia"] = [{"text":"impl&lt;'_&gt; TryFrom&lt;Option&lt;&amp;'_ str&gt;&gt; for CaseFold","synthetic":false,"types":[]},{"text":"impl&lt;'_&gt; TryFrom&lt;Option&lt;&amp;'_ [u8]&gt;&gt; for CaseFold","synthetic":false,"types":[]}];
+implementors["focaccia"] = [{"text":"impl TryFrom&lt;Option&lt;&amp;'_ str&gt;&gt; for CaseFold","synthetic":false,"types":[]},{"text":"impl TryFrom&lt;Option&lt;&amp;'_ [u8]&gt;&gt; for CaseFold","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
